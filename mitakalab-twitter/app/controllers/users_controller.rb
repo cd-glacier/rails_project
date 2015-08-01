4e393d57301cfc7efba0b5173439b0a9e2d117bb
hyoga <hyoga0216@gmail.com>
2015-08-01 13:40:14 +0900

    finish mitakalabTwitter

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -3,17 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
-	@user = Hash.new
-	if params[:username] == 'mayoshima'
-		@user[:name] = 'Shohei Aoki'
-		@user[:username] = 'moyashima'
-		@user[:location] = 'Tottori, Japan'
-		@user[:about] = 'Nice to meet you.a'
-	elsif params[:username] == 'ryooopan'
-		@user[:name] = 'Ryo Suzuki'
-		@user[:username] = 'ryooopan'
-		@user[:location] = 'Kanagawa, Japan'
-		@user[:about] = 'Hello, I am Ryo..'
+	if params[:username] == 'ryooopan'
+		@user = User.find_by(:username => 'ryooopan')
+	elsif params[:username] == 'moyahima'
+		@user = User.find_by(:username => 'moyahima')
 	end
   end
 end
--- mitakalab-twitter/config/routes.rb
+++ mitakalab-twitter/config/routes.rb
@@ -1,6 +1,15 @@
 MitakalabTwitter::Application.routes.draw do
+  get "tweets/index"
+  get "tweets/show"
+  get "tweets/new"
   get "users/index"
   get "users/show/:username" => "users#show"
+
+  get "tweets/index"
+  get "tweets/show"
+  get "tweets/new"
+  post "tweets" => "tweets#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
