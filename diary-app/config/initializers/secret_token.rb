ba79efc749301bd7f17000119f5f87c377ae4830
hyoga <hyoga0216@gmail.com>
2015-08-12 13:56:40 +0900

    create diary-app

---
+++ diary-app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiaryApp::Application.config.secret_key_base = 'd2a5781a555f89b5a3e7f4630119ddb5c3e22085774c55c4fc91aca0f5a0e6cb6548520b941f640622b2e32dc93a7d47bf849c0a6bfff725468f1bde0a8de3cc'
